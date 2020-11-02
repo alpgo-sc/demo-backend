cd2470fe452e7467244a8629518ae0c6de5c9718
张轩志 <zhangxuanzhi@zhangxuanzhideMacBook-Pro.local>
2020-11-02 22:00:50 +0800

    feat(init): init  spring boot

---
+++ src/test/java/pro/elegy/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.elegy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
